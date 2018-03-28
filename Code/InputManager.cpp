c4fb9e0f41455538f668e270edfd30a90895fcda
Emma Parsley <40206111@live.napier.ac.uk>
2018-03-28 23:53:51 +0100

    Simple string conversion -Maps That translates all playstation buttons and many keyboard keys into strings

--- Code/InputManager.cpp
+++ Code/InputManager.cpp
@@ -31,6 +31,63 @@ V Axis - L2 -100 not pressed, 100 fully pressed
 U Axis - R2 -100 not pressed, 100 fully pressed
 */
 
+///INPUT MANAGER CLASS///
+
+const std::map<sf::Keyboard::Key, std::string> InputManager::keyboardControls = {
+	{ sf::Keyboard::LShift, "LEFT SHIFT" },
+{ sf::Keyboard::RShift, "RIGHT SHIFT" },
+{ sf::Keyboard::LControl, "LEFT CONTROL" },
+{ sf::Keyboard::RControl, "RIGHT CONTROL" },
+{ sf::Keyboard::LAlt, "LEFT ALT" },
+{ sf::Keyboard::RAlt, "RIGHT ALT" },
+{ sf::Keyboard::Tab, "TAB" },
+{ sf::Keyboard::Return, "ENTER" },
+{ sf::Keyboard::BackSpace, "BACKSPACE" },
+{ sf::Keyboard::F1, "F1" },
+{ sf::Keyboard::F2, "F2" },
+{ sf::Keyboard::F3, "F3" },
+{ sf::Keyboard::F4, "F4" },
+{ sf::Keyboard::F5, "F5" },
+{ sf::Keyboard::F6, "F6" },
+{ sf::Keyboard::F7, "F7" },
+{ sf::Keyboard::F8, "F8" },
+{ sf::Keyboard::F9, "F9" },
+{ sf::Keyboard::F10, "F10" },
+{ sf::Keyboard::F11, "F11" },
+{ sf::Keyboard::F12, "F12" },
+{ sf::Keyboard::Home, "HOME" },
+{ sf::Keyboard::End, "END" },
+{ sf::Keyboard::PageUp, "PAGE UP" },
+{ sf::Keyboard::PageDown, "PAGE DOWN" },
+{ sf::Keyboard::Delete, "DEL" },
+{ sf::Keyboard::Left, "LEFT ARROW" },
+{ sf::Keyboard::Right, "RIGHT ARROW" },
+{ sf::Keyboard::Up, "UP ARROW" },
+{ sf::Keyboard::Down, "DOWN ARROW" },
+{ sf::Keyboard::Menu, "MENU" },
+{ sf::Keyboard::Pause, "PAUSE" },
+{ sf::Keyboard::Num0, "0" },
+{ sf::Keyboard::Num1, "1" },
+{ sf::Keyboard::Num2, "2" },
+{ sf::Keyboard::Num3, "3" },
+{ sf::Keyboard::Num4, "4" },
+{ sf::Keyboard::Num5, "5" },
+{ sf::Keyboard::Num6, "6" },
+{ sf::Keyboard::Num7, "7" },
+{ sf::Keyboard::Num8, "8" },
+{ sf::Keyboard::Num9, "9" },
+{ sf::Keyboard::Numpad0, "NUMPAD 0" },
+{ sf::Keyboard::Numpad1, "NUMPAD 1" },
+{ sf::Keyboard::Numpad2, "NUMPAD 2" },
+{ sf::Keyboard::Numpad3, "NUMPAD 3" },
+{ sf::Keyboard::Numpad4, "NUMPAD 4" },
+{ sf::Keyboard::Numpad5, "NUMPAD 5" },
+{ sf::Keyboard::Numpad6, "NUMPAD 6" },
+{ sf::Keyboard::Numpad7, "NUMPAD 7" },
+{ sf::Keyboard::Numpad8, "NUMPAD 8" },
+{ sf::Keyboard::Numpad9, "NUMPAD 9" },
+};
+
 InputManager::InputManager()
 {
 	ControlSystem pscontroller;
@@ -54,6 +111,23 @@ InputManager::InputManager()
 		cout << "Has a z-axis: " << hasZ << std::endl;
 	}
 }
+
+const std::map<InputManager::PS4, std::string> InputManager::ps4Controls = {
+	{InputManager::SQUARE, "SQUARE" },
+	{InputManager::X, "X" },
+	{InputManager::O, "CIRCLE" },
+	{InputManager::TRIANGLE, "TRIANGLE" },
+	{InputManager::L1, "L1" },
+	{InputManager::R1, "R1" },
+	{InputManager::L2, "L2" },
+	{InputManager::R2, "R2" },
+	{InputManager::SELECT, "SELECT" },
+	{InputManager::START, "START" },
+	{InputManager::LEFTA, "LEFT ANALOGUE" },
+	{InputManager::RIGHTA, "RIGHT ANOLOGUE" },
+	{InputManager::PS, "PLAY STATION" },
+	{InputManager::TOUCH, "TOUCH PAD" },
+};
 
 void InputManager::ButtonDebug()
 {
--- Code/InputManager.h
+++ Code/InputManager.h
@@ -1,26 +1,51 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 #include <map>
+#include <string>
+#include <bitset>
 
+//Control System struct
 struct ControlSystem {
-	std::map<unsigned int, unsigned int> controls;
-	string controlType;
+	//map of controls
+	std::map<unsigned int, std::pair<unsigned int, unsigned int>> controls;
+	//string for control type
+	std::string controlType;
 };
 
+//Input Manager Class
 class InputManager
 {
 public:
-	static enum PS4 { SQUARE, X, O, TRIANGLE, L1, R1, L2, R2, SELECT, START, LEFTA, RIGHTA, PS, TOUCH};
+	//PS4 Enum
+	static enum PS4 { SQUARE, X, O, TRIANGLE, L1, R1, L2, R2, SELECT, START, LEFTA, RIGHTA, PS, TOUCH, NONE = -1};
+	//Enum for actions
 	static enum Action { LEFT, RIGHT, SVM, JUMP, AIM, FIRE, SHIELD, ACTIVE };
+
+	//string Controls
+	static const std::map<PS4, std::string> ps4Controls;
+	static const std::map<sf::Keyboard::Key, std::string> keyboardControls;
+
+	//Control Systems
+	std::map<std::string, ControlSystem> keyMaps;
+	
+	//Constructor and Destructor
 	InputManager();
 	~InputManager() = default;
+
+	//methods
 	void Update(double dt);
+	void Remap(sf::RenderWindow &window, Action action, bool primary, std::string mapKey);
 	bool GetButtonDown(unsigned int button);
 	bool GetButtonHeld(unsigned int button);
 	bool GetButtonReleased(unsigned int button);
 protected:
-	bool buttonDown[14];
-	bool buttonHeld[14];
-	bool buttonReleased[14];
+	std::bitset<16> buttonDown;
+	std::bitset<16> buttonHeld;
+	std::bitset<16> buttonReleased;
+	
 	ControlSystem activeControls;
+
+	//methods
 	void ButtonDebug();
+	void CreateControlers();
 };
