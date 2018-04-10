61da5fa27447699afe62af50d73b0ce0d96e0226
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2018-04-11 00:54:10 +0100

    Initial work on TextGridScene.

--- Code/scenes/TextGridScene.cpp
+++ Code/scenes/TextGridScene.cpp
@@ -5,7 +5,46 @@
 using namespace sf;
 using namespace std;
 
-///ABSTRACT TEXT SCENE///
+///ABSTRACT TEXT GRID SCENE///
+
+TextGridScene::TextGridScene() {
+	texts.push_back(&text);
+	columns = texts.size();
+	rows = texts[0]->size();
+
+}
+
+void TextGridScene::SpreadRatios(int count, std::vector<int>& ratios) {
+	for (int& val : ratios) {
+		val = 0;
+	}
+	ChangeOtherRatios(true, -1, 100, ratios);
+}
+
+// increase - True adds value, false takes it away
+// index - the ratio to avoid, set to -1 to avoid none
+// value - the value to distribute or remove
+// ratios - the set of ratios to edit
+void TextGridScene::ChangeOtherRatios(bool increase, int index, int value, std::vector<int>& ratios) {
+	int k = (increase ? 1 : -1);
+	int total = 0;
+	while (total < value) {
+		int change = (value - total) / (ratios.size() - (index >= 0 ? 1 : 0));
+		for (int i = 0; i < ratios.size() && total < value; ++i) {
+			if (i != index) {
+				total += ChangeRatio(ratios[i], k * change);
+			}
+		}
+	}
+}
+
+int TextGridScene::ChangeRatio(int& ratio, int change) {
+	if (ratio + change < minRatio) {
+		change = minRatio - ratio;
+	}
+	ratio += change;
+	return change;
+}
 
 //Update method
 void TextGridScene::Update(double dt)
@@ -78,52 +117,66 @@ void TextGridScene::Update(double dt)
 //render Method
 void TextGridScene::Render()
 {
-	CalculateSpace();
-	//set text positions to fit screen
-	/*text[0].setPosition((Renderer::GetWindow().getSize().x * 0.5f) - (text[0].getLocalBounds().width), (Renderer::GetWindow().getSize().y * 0.5f) - (text[0].getLocalBounds().height * 3));
-	text[1].setPosition((Renderer::GetWindow().getSize().x * 0.5f) - (text[1].getLocalBounds().width), Renderer::GetWindow().getSize().y * 0.5f - (text[1].getLocalBounds().height));
-	text[2].setPosition((Renderer::GetWindow().getSize().x * 0.5f) - (text[2].getLocalBounds().width), (Renderer::GetWindow().getSize().y * 0.5f) + (text[2].getLocalBounds().height));*/
-	//render text
-	for (int i = 0; i < textAmount; ++i)
-	{
-		text[i].setCharacterSize(Renderer::GetWindow().getSize().x / 10);
+	Vector2u screenSize = Renderer::GetWindow().getSize();
+	float yPos = 0.0f;
+	float xPos = 0.0f;
+	Vector2f border = Vector2f((float)screenSize.x / 100.0f, (float)screenSize.y / 100.0f);
+	for (int y = 0; y < rows; ++y) {
+		float ySpace = (float)screenSize.y * (float)rowRatios[y] / 100.0f;
+		float yMid = yPos + ySpace / 2.0f;
+		for (int x = 0; x < columns; ++x) {
+			float xSpace = (float)screenSize.x * (float)columnRatios[x] / 100.0f;
+			float xMid = xPos + xSpace / 2.0f;
 
-		float xval = (Renderer::GetWindow().getSize().x * 0.25);
-		int co = i - textAmount / 2;
-		float yval = Renderer::GetWindow().getSize().y * 0.5f;
-		yval = yval + (co * space);
-		text[i].setPosition(Vector2f(xval, yval) - Vector2f(text[i].getLocalBounds().left, text[i].getLocalBounds().top));
-		if (false) {
-			Vector2f globPos = Vector2f(text[i].getGlobalBounds().left, text[i].getGlobalBounds().top);
-			Vector2f posPos = text[i].getPosition();
-			Vector2f locPos = Vector2f(text[i].getLocalBounds().left, text[i].getLocalBounds().top);
+			Vector2f localpos = Vector2f(texts[x]->at(y).getLocalBounds().left, texts[x]->at(y).getLocalBounds().top);
+
+			texts[x]->at(y).setPosition(Vector2f(xMid, yMid) - localpos);
+			Renderer::Queue(&texts[x]->at(y));
+
+
+			xPos += xSpace; // At end of loop only
 		}
-
-		Renderer::Queue(&text[i]);
+		yPos += ySpace; // At end of loop only
 	}
+
+
+	//render text
+	//for (int i = 0; i < textAmount; ++i)
+	//{
+	//	text[i].setCharacterSize(Renderer::GetWindow().getSize().x / 10);
+
+	//	float xval = (Renderer::GetWindow().getSize().x * 0.25);
+	//	int co = i - textAmount / 2;
+	//	float yval = Renderer::GetWindow().getSize().y * 0.5f;
+	//	yval = yval + (co * space);
+	//	//set text positions to fit screen
+	//	text[i].setPosition(Vector2f(xval, yval) - Vector2f(text[i].getLocalBounds().left, text[i].getLocalBounds().top));
+
+	//	Renderer::Queue(&text[i]);
+	//}
 }
 
-void TextGridScene::CalculateSpace() {
-	int count = 8; // Number to fit on screen by default
+void TextGridScene::CalculateSpaceX() {
+	int count = 3; // Number to fit on screen by default
 	float gap = Renderer::GetWindow().getSize().y / (textAmount < count ? (float)count + 1.0f : textAmount + 1);
 	space = gap;
 }
 
-void TextGridScene::ChangeCurrent(int value)
+void TextGridScene::ChangeCurrentX(int value)
 {
-	int lastCurrent = current;
+	int lastX = currentX;
 	if (value < 0) {
-		current = textAmount + (value % textAmount);
+		currentX = rows + (value % rows);
 	}
 	else if (value >= textAmount) {
-		current = value % textAmount;
+		currentX = value % rows;
 	}
 	else {
-		current = value;
+		currentX = value;
 	}
-	if (current != lastCurrent)
+	if (currentX != lastX)
 	{
-		text[lastCurrent].setColor(sf::Color::White);
-		text[current].setColor(sf::Color::Yellow);
+		texts[lastX]->at(current).setColor(sf::Color::White);
+		texts[currentX]->at(current).setColor(sf::Color::Yellow);
 	}
 }
--- Code/scenes/TextGridScene.h
+++ Code/scenes/TextGridScene.h
@@ -1,20 +1,42 @@
 #pragma once
-#include "Scene.h"
+#include "TextScene.h"
 #include <SFML/Graphics.hpp>
 
-class TextGridScene : public Scene
+class TextGridScene : public TextScene
 {
 protected:
-	std::vector<sf::Text> text;
-	int textAmount;
-	float space;
-	//currently selected
-	int current = 0;
-	std::shared_ptr<Scene> previousScene;
+	int currentX = 0;
+	std::vector<std::vector<sf::Text>*> texts;
+	int columns;
+	int rows;
+	std::vector<int> columnRatios;
+	int columnTotal = 0;
+	std::vector<int> rowRatios;
+	int rowTotal = 0;
+	int minRatio = 5;
+	int maxIndex = 100 / minRatio;
+	bool stealUp = false;
 
-	void CalculateSpace();
-	void ChangeCurrent(int);
+	void ChangeCurrentX(int);
+	void CalculateSpaceX();
+
+	void SpreadRatios(int count, std::vector<int>& ratios);
+	void ChangeOtherRatios(bool increase, int index, int value, std::vector<int>& ratios);
+	int ChangeRatio(int& ratio, int change);
+
 public:
+	int GetRowRatio(int row) { return rowRatios[row]; }
+	int GetRowCount() { return rows; }
+	int GetColumnRatio(int column) { return columnRatios[column]; }
+	int GetColumnCount() { return columns; }
+	sf::Text& GetElement(int column, int row) { return texts[column]->at(row); }
+
+	void SetRowRatio(int column, int ratio);
+	void SetRowCount(int value);
+	void SetColumnRatio(int column, int ratio);
+	void SetColumnCount(int value);
+	void SetElement(int column, int row, const sf::Text&);
+
 	TextGridScene() = default;
 	void Update(double dt) override;
 	void Render() override;
